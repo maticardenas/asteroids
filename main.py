02ad0fa3099e25aa2ea6c07a233a3d8ae83ad6f3
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-11-10 22:27:18 +0100

    feat: using pygame groups

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     plyr = Player(x=SCREEN_WIDTH // 2, y=SCREEN_HEIGHT // 2)
 
     while True:
@@ -20,9 +23,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Color("black"))
-        plyr.draw(screen)
-        plyr.update(dt)
+        for pl in updatable:
+            pl.update(dt)
+        for pl in drawable:
+            pl.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -38,4 +38,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
        
+    def move(self, dt: float) -> None:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
